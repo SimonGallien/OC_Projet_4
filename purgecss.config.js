ef71bccda34611b000176be0aae2c738b36f32de
Simon Gallien <simongallien@orange.fr>
2024-10-05 17:55:32 +0200

    Utilisation de purgeCSS et suppression de lignes bundle bootstrap

---
+++ purgecss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        'index.html'
+    ],
+    css: [
+        // './assets/bootstrap/bootstrap.css',
+        // './assets/css/style.css',
+        './assets/css/modal.css',
+    ],
+    keyframes: true,
+    output: './assets/purified/modal.css',
+    rejected: true,
+    variables: true,
+  }
