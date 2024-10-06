db5419c0040aa919fff614ee9f5344abe7f861e7
Simon Gallien <simongallien@orange.fr>
2024-10-07 00:01:08 +0200

    Fichier allégés, minifiés

--- assets/script/modal/modal.js
+++ assets/script/modal/modal.js
@@ -2,7 +2,7 @@
 /****** Ce fichier contient le code javascript de la modale ******/
 /*****************************************************************/
 
-import {openModal} from "./modalFunctions.js";
+import {openModal} from "./modalFunctions.min.js";
 
 document.querySelectorAll('.js-modal').forEach(linkImg => {
     linkImg.addEventListener('click', openModal)
---
+++ purgecss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        'index.html'
+    ],
+    css: [
+        './assets/bootstrap/minify/bootstrap.min.css',
+        './assets/css/minify/style.min.css',
+        './assets/css/minify/modal.min.css',
+    ],
+    keyframes: true,
+    rejected: true,
+    variables: true,
+  }
